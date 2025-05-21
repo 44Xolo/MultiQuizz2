a85f9353650e34224c9ac4f27a9b258e1ef43bf3
lab_services_student <hlomphoxolo4@gmail.com>
2025-05-21 08:14:21 +0200

    Initial commit

---
+++ app/src/main/java/vcmsa/hlompho/multiquizz2/MainActivity.kt
@@ -0,0 +1,33 @@
+package vcmsa.hlompho.multiquizz2
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        //code here
+
+        val btStartQuiz = findViewById<Button>(R.id.btStartQuiz)
+        btStartQuiz.setOnClickListener {
+            val intent = Intent(this, MultiChoicePg1::class.java)
+            startActivity(intent)
+
+            //code ends here
+        }
+
+
+    }
+}
---
+++ app/src/main/java/vcmsa/hlompho/multiquizz2/MultiChoicePg1.kt
@@ -0,0 +1,20 @@
+package vcmsa.hlompho.multiquizz2
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MultiChoicePg1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_multi_choice_pg1)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
